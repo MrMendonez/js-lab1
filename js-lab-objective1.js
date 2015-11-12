af40d1e8feb78771f5333ac3a0e55eae4fe52951
MrMendonez <dmendonez@gmail.com>
2015-11-12 17:06:43 -0500

    declared and assigned var value in one step instead of two.

--- js-lab-objective1.js
+++ js-lab-objective1.js
@@ -3,9 +3,7 @@
 // 1. Write a function to determine if a given variable is a 
 // string or not
 
-var value
-
-value = prompt("Type something in.");
+var value = prompt("Type something in.");
 
 console.log("You typed ", value);
 console.log("The datatype is: ", typeof value);
