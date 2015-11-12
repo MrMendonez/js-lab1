41480f379f744c5b0fcef2b7273e5076f7309566
MrMendonez <dmendonez@gmail.com>
2015-11-12 13:15:54 -0500

    initial commit. finished 3 lab objectives from 11/12

---
+++ js-lab-objective1.js
@@ -0,0 +1,11 @@
+// Lab 11/11/2015
+
+// 1. Write a function to determine if a given variable is a 
+// string or not
+
+var value
+
+value = prompt("Type something in.");
+
+console.log("You typed ", value);
+console.log("The datatype is: ", typeof value);
