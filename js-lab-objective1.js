9d0ab14797e79157522be562d2074f18797d23c5
MrMendonez <dmendonez@gmail.com>
2015-11-12 20:05:42 -0500

    redid lab objective 1 so that it works in browser not console.

--- js-lab-objective1.js
+++ js-lab-objective1.js
@@ -1,9 +1,15 @@
-// Lab 11/11/2015
+// Lab 11/11/2015 by Darryl Mendonez
 
 // 1. Write a function to determine if a given variable is a 
 // string or not
 
-var value = prompt("Type something in.");
+function dataType() {
 
-console.log("You typed ", value);
-console.log("The datatype is: ", typeof value);
+  var value = prompt("Type something in and I will tell you what kind of data type it is.");
+
+  if ((value === "") || (isNaN(value))) {
+    alert("You typed '" + value + "' which is a string.");
+  } else {
+    alert("You typed '" + value + "' which is a number.")
+  }
+}
