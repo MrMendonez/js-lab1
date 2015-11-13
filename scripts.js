c3b2f71b0dfaab7dc674ce86066b2ac9f02663c7
MrMendonez <dmendonez@gmail.com>
2015-11-12 23:53:50 -0500

    failed attempt at using arrays to contatenate =(

--- scripts.js
+++ scripts.js
@@ -31,3 +31,35 @@ function comparison() {
   }
 }
 
+// 3. Write a function that concatenates n number of strings together
+
+function concatenator() {
+  i = 0;
+  n = 1;
+  var ok = true;
+  var list = [];
+  list[i] = prompt("What is your favorite movie?")
+  while (ok === true) {
+    if (confirm("Ok, tell me others by pressing OK or press CANCEL to stop.")) {
+      list[i+n] = prompt("What is another of your favorite movies?");
+      n += 1
+    } else {
+      ok = false;
+      console.log(list);
+      i = 0
+      if (list.length === 1) {
+        var result = "Ok, so your favorite movie is " + list[i] + "."
+        alert(result);
+      } else {
+        while (i < list.length) {
+          var n = list[i] + ", "
+          n += n
+          i++
+          console.log(n)
+          var result = "Ok, so your favorite movies are " + n
+          alert(result)
+        }
+      }
+    }
+  }
+}
